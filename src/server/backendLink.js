d875a6cb502e266a95374457e56a576d0f50525c
Sayonil Mitra <sayonilmitra@gmail.com>
2023-03-25 17:09:51 +0530

    changed back end link

--- src/server/backendLink.js
+++ src/server/backendLink.js
@@ -1,3 +1,4 @@
-const backendLink = 'http://localhost:8000'
+//const backendLink = 'http://localhost:8000'
+const backendLink = 'https://notes-app-sayonil.onrender.com'
 
 module.exports = backendLink
